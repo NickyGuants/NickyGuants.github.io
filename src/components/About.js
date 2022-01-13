afb5cfa4275041f2516259a0d99b386a41922511
NickyGuants <nicholasguantai528@gmail.com>
2022-01-13 19:17:56 +0300

    hero section design

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,11 @@ import React from "react";
 const About = () => {
   return (
     <section className="about">
-      <h1>My Skills</h1>
+      <h1>Skills</h1>
       <div className="skills">
         <div className="card">
           <h2>Front-End Development</h2>
+          {/* <img src="../../public/images/frontend.png" alt="frontend-img" /> */}
           <p>User experience and usable interfaces is my biggest concern</p>
           <div>
             <h3>Tech Stack</h3>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,41 +6,47 @@ const Hero = () => {
     <section className="hero">
       <div className="branding">
         <div className="profile">
-          <img
-            id="profile"
-            src="https://avatars.githubusercontent.com/u/51677808?v=4"
-            alt="Profile"
-          />
-          <h1>Hi, I'm Guantai Nicholas</h1>
-          <h2>I am a full-stack developer from Kenya</h2>
-          <p>
-            I am passionate about solving problems using various technologies.
-          </p>
-          <div className="social-media-links">
-            <h3>Let's Connect</h3>
-            <div className="links">
-              <a
-                href="https://twitter.com/Nicky_guants"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/nicholasguantai/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiFillLinkedin />
-              </a>
-              <a
-                href="https://github.com/NickyGuants"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiFillGithub />
-              </a>
-            </div>
+          <div className="profile-pic">
+            <img
+              id="profile"
+              src="https://avatars.githubusercontent.com/u/51677808?v=4"
+              alt="Profile"
+            />
+          </div>
+
+          <div className="profile-text">
+            <h1>Hi, I'm Guantai Nicholas</h1>
+            <h2>I am a full-stack developer from Kenya</h2>
+            <p>
+              I am passionate about solving problems using various technologies.
+            </p>
+          </div>
+        </div>
+
+        <div className="social-media-links">
+          <h3>Let's Connect</h3>
+          <div className="links">
+            <a
+              href="https://twitter.com/Nicky_guants"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiOutlineTwitter />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/nicholasguantai/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillLinkedin />
+            </a>
+            <a
+              href="https://github.com/NickyGuants"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillGithub />
+            </a>
           </div>
         </div>
       </div>
