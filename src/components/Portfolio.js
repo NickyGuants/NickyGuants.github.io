e2d2518e4f5220065473e3db0daa70fd4521f9f7
NickyGuants <nicholasguantai528@gmail.com>
2022-06-10 15:14:30 +0300

    projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,8 +2,62 @@ import React from "react";
 
 const Portfolio = () => {
   return (
-    <section className="portfolio">
+    <section className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
+      <div className="projects">
+        <div className="project">
+          <div className="project-cover">
+            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2022/02/arnold-francisca-f77Bh3inUpE-unsplash.jpg" alt="pic" />
+          </div>
+          <div className="project-desc">
+            <h2>Attachment Portal</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi rem dolorum totam? Quis modi animi nesciunt expedita vero, perspiciatis, quam voluptates porro unde, et incidunt ipsum atque. Laboriosam, dolores fugit.</p>
+          </div>
+          <div className="project-links">
+              <a href="#link">Github Code</a>
+              <a href="#link">Live Site</a>
+            </div>
+        </div>
+        <div className="project">
+          <div className="project-cover"> 
+            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2022/02/arnold-francisca-f77Bh3inUpE-unsplash.jpg" alt="pic" />
+          </div> 
+          <div className="project-desc">
+            <h2>E-commerce App</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, cumque sed. Nisi sequi maiores reiciendis nam debitis, asperiores dolore nulla dolor non veritatis, recusandae ducimus eius rem eos nesciunt iure!</p>
+          </div>
+          <div className="project-links">
+              <a href="#link">Github Code</a>
+              <a href="#link">Live Site</a>
+            </div>
+        </div>
+        <div className="project">
+          <div className="project-cover">
+            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2022/02/arnold-francisca-f77Bh3inUpE-unsplash.jpg" alt="pic" />
+          </div>
+          <div className="project-desc">
+            <h2>Project Management system</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, eaque voluptatum natus nulla expedita porro voluptates eius ut, corporis obcaecati quo repellat repellendus cumque cum officiis dolore fuga asperiores quidem!</p>
+          </div>
+          <div className="project-links">
+              <a href="#link">Github Code</a>
+              <a href="#link">Live Site</a>
+            </div>
+        </div>
+        <div className="project">
+          <div className="project-cover">
+            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2022/02/arnold-francisca-f77Bh3inUpE-unsplash.jpg" alt="pic" />
+          </div>
+          <div className="project-desc">
+            <h2>App</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut deserunt quibusdam corrupti enim exercitationem obcaecati, dolorum, aliquam assumenda aspernatur eaque asperiores illo deleniti nemo magni animi dignissimos laudantium suscipit autem.</p>
+          </div>
+          <div className="project-links">
+              <a href="#link">Github Code</a>
+              <a href="#link">Live Site</a>
+            </div>
+        </div>
+      </div>
     </section>
   );
 };
